70debb4ebb07780845cf54df40491b722bd1a6d5
Giovanna444 <giovanna.imamura.portella@gmail.com>
2026-02-20 20:37:18 -0300

    Add files via upload

---
+++ Untitled-2 - fundamentos algoritmos.py
@@ -0,0 +1,6 @@
+a=int(input("Digite um numero"))
+print(a)
+if a>0:
+    print("Esse numero eh positivo!")
+else:
+    print("Esse numero nao eh positivo!")
---
+++ fundamentos de algoritmos.py
@@ -0,0 +1,8 @@
+n1=int(input("Digite um numero"))
+print(n1)
+n2=int(input("Digite outro numero"))
+print(n2)
+if n1>n2:
+    print("maior", n1)
+else:
+    print("maior", n2)
